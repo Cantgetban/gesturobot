c9e2a375f331682810423ca252733c3e9beefba9
Sahar Rofe <Saharofe01@gmail.com>
2023-03-19 20:03:37 +0200

    added registaration form

--- src/pages/gesturemanagement/gesturemanagement.js
+++ src/pages/gesturemanagement/gesturemanagement.js
@@ -1,5 +1,47 @@
+import React, { useState } from 'react';
+
 function GestureManagement() {
-  return <div>This is GestureManagement page!</div>;
-}
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+  
+    const handleUsernameChange = (event) => {
+      setUsername(event.target.value);
+    };
+  
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+  
+    const handleLogin = (event) => {
+      event.preventDefault();
+  
+      // Define the regular expression to match the password format
+      const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  
+      if (!passwordRegex.test(password)) {
+        setErrorMessage('Password must be at least 8 characters and contain at least one letter and one number');
+      } else {
+        // Submit the form or perform any other necessary actions
+        console.log('Form submitted');
+      }
+    };
+
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username-input">Username:</label>
+          <input id="username-input" type="text" value={username} onChange={handleUsernameChange} required />
+        </div>
+        <div>
+          <label htmlFor="password-input">Password:</label>
+          <input id="password-input" type="password" value={password} onChange={handlePasswordChange} required />
+        </div>
+        {errorMessage && <div>{errorMessage}</div>}
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
 
 export default GestureManagement;
