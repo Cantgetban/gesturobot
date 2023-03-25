ed90c853246ad4e9b9add629c7cc31bd870bfb56
Sahar Rofe <Saharofe01@gmail.com>
2023-03-25 23:19:00 +0300

    added navigate at the main page

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -1,16 +1,32 @@
 
 import { Translations } from "../language-management/Translations.js";
+import {useNavigate} from 'react-router-dom';
 
 function MainPage() {
+
+  let navigate = useNavigate();
+
+  const moveToGestureManagement = () => {
+    navigate('/GestureManagement');
+  }
+
+  const moveToMovmentLibrary = () => {
+    navigate('/MovementLib');
+  }
+
+  const moveToGestureLabeling = () => {
+    navigate('/GestureTag');
+  }
+
   return (
     <Translations>
       {({ translate }) => (
         <div>
           <h1>{translate("Welcome to my landing page")}</h1>
           <div className="row">
-            <button className="btn btn-primary">{translate("Gesture Management")}</button>
-            <button className="btn btn-secondary">{translate("Movment Library")}</button>
-            <button className="btn btn-danger">{translate("Gesture Labeling")}</button>
+            <button onClick={moveToGestureManagement} className="btn btn-primary">{translate("Gesture Management")}</button>
+            <button onClick={moveToMovmentLibrary} className="btn btn-secondary">{translate("Movment Library")}</button>
+            <button onClick={moveToGestureLabeling} className="btn btn-danger">{translate("Gesture Labeling")}</button>
           </div>
         </div>
       )}
