ca996416e67bbf80ae04eaaa9685d984d256931d
Sahar Rofe <Saharofe01@gmail.com>
2023-03-26 16:20:59 +0300

    added support from en/he at the movment component

--- src/components/movment/movment.js
+++ src/components/movment/movment.js
@@ -1,31 +1,42 @@
 import React, { useRef } from "react";
+import { useContext } from "react";
+import { LanguageContext } from "../../pages/language-management/LanguageContext";
+import { Translations } from "../../pages/language-management/Translations";
 
 const Movement = ({movement }) => {
   const videoRef = useRef(null);
+
+  const { language } = useContext(LanguageContext)
+  const name = language === "en" ? movement.name : movement.hebrewName;
+  const description = language === "en" ? movement.description : movement.hebrewDescription; 
 
   const handleButtonClick = () => {
     videoRef.current.play();
   };
 
   return (
+    <Translations>
+        {({ translate }) => (
     <div className="card" style={{ width: "18rem" }}>
       <div className="embed-responsive embed-responsive-16by9">
         <iframe
           ref={videoRef}
-          title={movement.name}
+          title={name}
           className="embed-responsive-item"
           src={movement.videoSrc}
           allowFullScreen
         ></iframe>
       </div>
       <div className="card-body">
-        <h5 className="card-title">{movement.name}</h5>
-        <p className="card-text">{movement.description}</p>
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
         <button className="btn btn-primary" onClick={handleButtonClick}>
-          Start Movement
+          {translate("Start Movement")}
         </button>
       </div>
     </div>
+        )}
+    </Translations>
   );
 };
 
