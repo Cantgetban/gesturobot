5e70b98ec890a5bb3d1c2b15f7fec1cd02266ac3
Sahar Rofe <Saharofe01@gmail.com>
2023-04-09 18:50:02 +0300

    started working on "create gesture" file. not working yet. need to install react-dnd and react-dnd-html5-backend

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ import MainPage from "./pages/mainpage/MainPage";
 import UserLogin from "./pages/userLogin/userLogin";
 import MovementsLib from "./pages/movementslib/movementslib";
 import GestureTag from "./pages/gesturetag/gesturetag";
-import GestureManagement from "./pages/gestureManagement/gestureManagement";
+import GestureManagement from "./pages/gesturemanagement/gesturemanagement";
 import GestureDisplay from "./pages/gesturedisplay/GestureDisplay";
 import { LanguageSwitcher } from "./language-management/LanguageSwicher";
 import { LanguageContext } from "./language-management/LanguageContext";
+import Creategesture from "./pages/creategesture/creategesture";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 function App() {
   const { language } = useContext(LanguageContext);
   document.documentElement.setAttribute(
@@ -19,7 +23,7 @@ function App() {
   );
 
   return (
-    <div>
+      <DndProvider backend={HTML5Backend}>
       <div>
         <LanguageSwitcher />
       </div>
@@ -29,9 +33,10 @@ function App() {
         <Route path="/GestureTag" element={<GestureTag />} />
         <Route path="/UserLogin" element={<UserLogin />} />
         <Route path="/GestureManagement" element={<GestureManagement />} />
+        <Route path="/CreateGesture" element={<Creategesture />} />
         <Route path="/GestureDisplay" element={<GestureDisplay />} />
       </Routes>
-    </div>
+      </DndProvider>
   );
 }
 
--- src/components/movment/movment.js
+++ src/components/movment/movment.js
@@ -1,15 +1,25 @@
 import React, { useRef } from "react";
+import { useDrag } from "react-dnd";
 import { useContext } from "react";
 import { LanguageContext } from "../../language-management/LanguageContext";
 import { Translations } from "../../language-management/Translations";
 
-const Movement = ({ movement }) => {
+const Movement = ({ movement, draggable }) => {
   const videoRef = useRef(null);
-
+  
   const { language } = useContext(LanguageContext);
   const name = language === "en" ? movement.name : movement.hebrewName;
   const description =
     language === "en" ? movement.description : movement.hebrewDescription;
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "movement", // specify the type of the drag source
+    item: { movement },
+    canDrag: draggable,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
   const handleButtonClick = () => {
     videoRef.current.play();
@@ -18,22 +28,28 @@ const Movement = ({ movement }) => {
   return (
     <Translations>
       {({ translate }) => (
-        <div className="card" style={{ width: "18rem" }}>
-          <div className="embed-responsive embed-responsive-16by9">
-            <iframe
+        <div
+          className="card"
+          style={{ width: "18rem", opacity: isDragging ? 0.5 : 1 }}
+          ref={drag}
+        >
+          <span className="embed-responsive embed-responsive-16by9">
+            <video
               ref={videoRef}
               title={name}
               className="embed-responsive-item"
               src={movement.videoSrc}
-              allowFullScreen
-            ></iframe>
-          </div>
+              controls
+            />
+          </span>
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
             <p className="card-text">{description}</p>
-            <button className="btn btn-primary" onClick={handleButtonClick}>
-              {translate("Start Movement")}
-            </button>
+            {draggable && (
+              <button className="btn btn-primary" onClick={handleButtonClick}>
+                {translate("Start Movement")}
+              </button>
+            )}
           </div>
         </div>
       )}
