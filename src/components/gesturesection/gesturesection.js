b588f2430c5e8baa76defcb414810624704e395e
Sahar Rofe <Saharofe01@gmail.com>
2023-06-02 22:07:40 +0300

    added the option to delete gesture from desplay gestures

--- src/components/gesturesection/gesturesection.js
+++ src/components/gesturesection/gesturesection.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useRef, useContext} from 'react';
-import "./gesturesection.css";
-import { getAllGestures } from "../../databases/gesturesAPI";
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import './gesturesection.css';
+import { getAllGestures, deleteGesture } from '../../databases/gesturesAPI';
 import LoopOfMovements from '../loopOfMovements/loopOfMovements';
 import { LanguageContext } from '../../language-management/LanguageContext';
-
 
 function GestureSection(props) {
   const language = useContext(LanguageContext);
   const [gestures, setGestures] = useState([]);
+  const [hoveredGestureId, setHoveredGestureId] = useState(null);
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -19,14 +19,19 @@ function GestureSection(props) {
     fetchGestures();
   }, []);
 
+  const handleDeleteGesture = async (gestureId) => {
+    await deleteGesture(gestureId);
+    setGestures(gestures.filter(gesture => gesture.id !== gestureId));
+  };
+
   const filteredGestures = gestures.filter(gesture => {
     if (props.filterBy === 'name') {
       return gesture.creator[0].toLowerCase().includes(props.value.toLowerCase());
     } else if (props.filterBy === 'emotion') {
-      const temp = language == 'en' ? gesture.realLabel[0] : gesture.realLabel[1];
+      const temp = language === 'en' ? gesture.realLabel[0] : gesture.realLabel[1];
       return temp.toLowerCase().includes(props.value.toLowerCase());
     } else if (props.filterBy === 'type') {
-      const type = gesture.creator[1] === 0 ? "0" : "1";
+      const type = gesture.creator[1] === 0 ? '0' : '1';
       return type.includes(props.value.toLowerCase());
     } else if (props.filterBy === 'date') {
       return gesture.date.toLowerCase().includes(props.value.toLowerCase());
@@ -36,9 +41,19 @@ function GestureSection(props) {
 
   return (
     <div className="row">
-      {filteredGestures.map((gesture) => (
-        <div className="col-lg-4 col-sm-6 col-12 mb-4" key={gesture.id}>
+      {filteredGestures.map(gesture => (
+        <div
+          className="col-lg-4 col-sm-6 col-12 mb-4"
+          key={gesture.id}
+          onMouseEnter={() => setHoveredGestureId(gesture.id)}
+          onMouseLeave={() => setHoveredGestureId(null)}
+        >
           <div className="card">
+            {hoveredGestureId === gesture.id && (
+              <span className="delete-gesture" onClick={() => handleDeleteGesture(gesture.id)}>
+                &#10006; {/* Delete icon */}
+              </span>
+            )}
             <div className="card-video">
               <LoopOfMovements ids={gesture.movements} />
             </div>
